1264bb242ebfe664fddd48eb14b907fba388f124
suku <sukusovi@gmail.com>
2020-01-05 12:00:54 +0100

    Rename app to modules.

--- projects/starter/src/modules/app/app.module.ts
+++ projects/starter/src/modules/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core/core.module';
+import { SharedModule } from '../shared/shared.module';
+import { CoreModule } from '../core/core.module';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app.component';
 
-@NgModule({ 
+@NgModule({
   imports: [
     // angular
     BrowserAnimationsModule,
